61ef08bf6f0445009217e6206d34168a5bb23a27
Dandazine <ddaneshfar@my.bcit.ca>
2022-11-10 11:12:36 -0800

    fixed something in footer

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,12 +1,12 @@
-import React from 'react'
+import React from "react";
 
 const Footer = () => {
-    const date = new Date().getFullYear();
+  const date = new Date().getFullYear();
   return (
     <footer>
-        <p>&copy; {date} || Developed by: Reilly, Jed, Jalen and Danial</p>
+      <p>&copy; {date} || Developed by: Danial, Reilly, Jed, and Jalen</p>
     </footer>
-  )
-}
+  );
+};
 
-export default Footer
+export default Footer;
